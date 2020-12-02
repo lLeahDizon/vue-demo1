3a00810de1d27f2894f77006e246d61ac897924c
Lemon <516330797@qq.com>
2020-12-02 20:54:57 +0800

    create Demo.Vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import Demo from "./Demo.vue";
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  render(h) {
+    return h(Demo);
+  },
+});
