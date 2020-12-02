4d209844afa8464e344e46bf1a666593d664b308
Lemon <516330797@qq.com>
2020-12-02 23:23:44 +0800

    feat: 实现三种方式componentt

--- src/main.js
+++ src/main.js
@@ -1,12 +1,49 @@
-import Vue from "vue";
-
+const Vue = window.Vue;
 Vue.config.productionTip = false;
 
 import Demo from "./Demo.vue";
 
-new Vue({
-  el: "#app",
-  render(h) {
-    return h(Demo);
-  },
+Vue.component("Demo2", {
+  template: `
+  <div>demo2</div>
+  `,
 });
+
+new Vue({
+  components: {
+    Lemon: {
+      data() {
+        return { n: 0 };
+      },
+      template: `
+  <div>Lemon's n: {{n}}</div>
+  `,
+    },
+    Demo,
+  },
+  data() {
+    return {
+      n: 0,
+      array: [1, 2, 3, 4, 5, 6, 7, 8],
+    };
+  },
+  template: `
+  <div class="red">
+        {{n}}
+        <button @click="add">+1</button>
+        <Lemon/>
+        <Demo />
+        <hr>
+        {{filter()}}
+    </div>
+    `,
+  methods: {
+    add() {
+      this.n += 1;
+    },
+    filter() {
+      console.log("执行了 filter 函数");
+      return this.array.filter((i) => i % 2 === 0);
+    },
+  },
+}).$mount("#lemon");
